a84a08ad4ece1ada54799a88a8cc5be37e479701
cchalkias <cch@cchnet.gr>
2021-06-07 23:06:42 +0300

    Update weather.go

--- controller/weather.go
+++ controller/weather.go
@@ -1,105 +1,220 @@
-package controller
+package v2
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
-	"log"
 	"net/http"
-	"strconv"
+	"time"
 
-	"github.com/PuerkitoBio/goquery"
 	"github.com/gorilla/mux"
 	"github.com/E-PSN/weather-api/model"
-	"github.com/E-PSN/weather-api/parser"
 )
 
-var temperatureTags = []string{"body > pre > span:nth-child(3)", "body > pre > span:nth-child(2)"}
-var windTags = []string{"body > pre > span:nth-child(6)", "body > pre > span:nth-child(7)"}
-var descriptionTags = []string{"body > pre"}
-var temperatureForecastTags = [3][]string{{"body > pre >span:nth-child(17)", "body > pre > span:nth-child(16)"},
-	{"body > pre >span:nth-child(55)", "body > pre > span:nth-child(54)"},
-	{"body > pre >span:nth-child(91)", "body > pre > span:nth-child(90)"}}
-var windForecastTags = [3][]string{{"body > pre >span:nth-child(31)", "body > pre > span:nth-child(30)", "body > pre >span:nth-child(32)"},
-	{"body > pre >span:nth-child(68)", "body > pre > span:nth-child(67)", "body > pre > span:nth-child(69)"},
-	{"body > pre >span:nth-child(105)", "body > pre > span:nth-child(104)", "body > pre > span:nth-child(106)"}}
+const (
+	timeFormat = "2006-01-02"
+	wttrURL    = "http://wttr.in"
+)
+
+type wttrResponse struct {
+	CurrentCondition []currentCondition `json:"current_condition"`
+	Request          []struct {
+		Query string `json:"query"`
+		Type  string `json:"type"`
+	} `json:"request"`
+	Weather []struct {
+		Astronomy []struct {
+			MoonIllumination string `json:"moon_illumination"`
+			MoonPhase        string `json:"moon_phase"`
+			Moonrise         string `json:"moonrise"`
+			Moonset          string `json:"moonset"`
+			Sunrise          string `json:"sunrise"`
+			Sunset           string `json:"sunset"`
+		} `json:"astronomy"`
+		Date        string   `json:"date"`
+		Hourly      []hourly `json:"hourly"`
+		MaxtempC    string   `json:"maxtempC"`
+		MaxtempF    string   `json:"maxtempF"`
+		MintempC    string   `json:"mintempC"`
+		MintempF    string   `json:"mintempF"`
+		SunHour     string   `json:"sunHour"`
+		TotalSnowCm string   `json:"totalSnow_cm"`
+		UvIndex     string   `json:"uvIndex"`
+	} `json:"weather"`
+}
+
+type currentCondition struct {
+	FeelsLikeC      string `json:"FeelsLikeC"`
+	FeelsLikeF      string `json:"FeelsLikeF"`
+	Cloudcover      string `json:"cloudcover"`
+	Humidity        string `json:"humidity"`
+	ObservationTime string `json:"observation_time"`
+	PrecipMM        string `json:"precipMM"`
+	Pressure        string `json:"pressure"`
+	TempC           string `json:"temp_C"`
+	TempF           string `json:"temp_F"`
+	UvIndex         int    `json:"uvIndex"`
+	Visibility      string `json:"visibility"`
+	WeatherCode     string `json:"weatherCode"`
+	WeatherDesc     []struct {
+		Value string `json:"value"`
+	} `json:"weatherDesc"`
+	WeatherIconURL []struct {
+		Value string `json:"value"`
+	} `json:"weatherIconUrl"`
+	Winddir16Point string `json:"winddir16Point"`
+	WinddirDegree  string `json:"winddirDegree"`
+	WindspeedKmph  string `json:"windspeedKmph"`
+	WindspeedMiles string `json:"windspeedMiles"`
+}
+
+func (cc currentCondition) Temp(unit string) string {
+	switch unit {
+	case "u":
+		return cc.TempF + " °F"
+	default:
+		return cc.TempC + " °C"
+	}
+}
+
+func (cc currentCondition) Windspeed(unit string) string {
+	switch unit {
+	case "u":
+		return cc.WindspeedMiles + " mph"
+	default:
+		return cc.WindspeedKmph + " km/h"
+	}
+}
+
+type hourly struct {
+	DewPointC        string `json:"DewPointC"`
+	DewPointF        string `json:"DewPointF"`
+	FeelsLikeC       string `json:"FeelsLikeC"`
+	FeelsLikeF       string `json:"FeelsLikeF"`
+	HeatIndexC       string `json:"HeatIndexC"`
+	HeatIndexF       string `json:"HeatIndexF"`
+	WindChillC       string `json:"WindChillC"`
+	WindChillF       string `json:"WindChillF"`
+	WindGustKmph     string `json:"WindGustKmph"`
+	WindGustMiles    string `json:"WindGustMiles"`
+	Chanceoffog      string `json:"chanceoffog"`
+	Chanceoffrost    string `json:"chanceoffrost"`
+	Chanceofhightemp string `json:"chanceofhightemp"`
+	Chanceofovercast string `json:"chanceofovercast"`
+	Chanceofrain     string `json:"chanceofrain"`
+	Chanceofremdry   string `json:"chanceofremdry"`
+	Chanceofsnow     string `json:"chanceofsnow"`
+	Chanceofsunshine string `json:"chanceofsunshine"`
+	Chanceofthunder  string `json:"chanceofthunder"`
+	Chanceofwindy    string `json:"chanceofwindy"`
+	Cloudcover       string `json:"cloudcover"`
+	Humidity         string `json:"humidity"`
+	PrecipMM         string `json:"precipMM"`
+	Pressure         string `json:"pressure"`
+	TempC            string `json:"tempC"`
+	TempF            string `json:"tempF"`
+	Time             string `json:"time"`
+	UvIndex          string `json:"uvIndex"`
+	Visibility       string `json:"visibility"`
+	WeatherCode      string `json:"weatherCode"`
+	WeatherDesc      []struct {
+		Value string `json:"value"`
+	} `json:"weatherDesc"`
+	WeatherIconURL []struct {
+		Value string `json:"value"`
+	} `json:"weatherIconUrl"`
+	Winddir16Point string `json:"winddir16Point"`
+	WinddirDegree  string `json:"winddirDegree"`
+	WindspeedKmph  string `json:"windspeedKmph"`
+	WindspeedMiles string `json:"windspeedMiles"`
+}
+
+func (h hourly) Temp(unit string) string {
+	switch unit {
+	case "u":
+		return h.TempF + " °F"
+	default:
+		return h.TempC + " °C"
+	}
+}
+
+func (h hourly) Windspeed(unit string) string {
+	switch unit {
+	case "u":
+		return h.WindspeedMiles + " mph"
+	default:
+		return h.WindspeedKmph + " km/h"
+	}
+}
 
 // CurrentWeather gets the current weather to show in JSON format
+//
+// This endpoint uses wttr API with JSON response under the hood to make it
+// easier to handle with units and formats
 func CurrentWeather(w http.ResponseWriter, r *http.Request) {
+	city := mux.Vars(r)["city"]
+	uri := fmt.Sprintf("%s/%s?format=j1", wttrURL, city)
+	res, err := http.Get(uri)
 
-	var weather model.Weather
-	var err error
-
-	resp := getExternalWeather(getCity(r))
-	defer resp.Body.Close()
-
-	if resp.StatusCode == 200 {
-		err = parse(resp, &weather)
+	if err != nil {
+		errJSON, _ := json.Marshal(model.ErrorMessage{Message: err.Error()})
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(errJSON)
+		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	if err != nil {
+	if res.StatusCode != http.StatusOK {
+		errJSON, _ := json.Marshal(model.ErrorMessage{Message: "NOT_FOUND"})
 		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, string(toJSON(model.ErrorMessage{Message: "NOT_FOUND"})))
-	} else {
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, string(toJSON(weather)))
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(errJSON)
+		return
 	}
 
-}
+	defer res.Body.Close()
 
-func getCity(r *http.Request) string {
-	return mux.Vars(r)["city"]
-}
-
-func getExternalWeather(city string) *http.Response {
-	resp, err := http.Get("http://wttr.in/" + city + "?m")
-	if err != nil {
-		log.Fatal("Cannot open url: ", err)
-	}
-	return resp
-}
-
-func parse(resp *http.Response, weather *model.Weather) error {
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	weather.Description = parser.Parse(doc, descriptionTags)
-	weather.Temperature = parser.Parse(doc, temperatureTags)
-	if len(weather.Temperature) > 0 {
-		weather.Temperature = weather.Temperature + " °C"
+	var wttr wttrResponse
+	if err := json.NewDecoder(res.Body).Decode(&wttr); err != nil {
+		errJSON, _ := json.Marshal(model.ErrorMessage{Message: err.Error()})
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(errJSON)
+		return
 	}
 
-	weather.Wind = parser.Parse(doc, windTags)
-	if len(weather.Wind) > 0 {
-		weather.Wind = weather.Wind + " km/h"
-	}
-	if notFound(weather) {
-		return errors.New("NOT_FOUND")
-	}
+	var (
+		cc   = wttr.CurrentCondition[0]
+		unit = mux.Vars(r)["unit"]
+	)
 
-	for i := range weather.Forecast {
-		weather.Forecast[i].Day = strconv.Itoa(i + 1)
-		weather.Forecast[i].Temperature = parser.Parse(doc, temperatureForecastTags[i]) + " °C"
-		weather.Forecast[i].Wind = parser.Parse(doc, windForecastTags[i]) + " km/h"
+	response := model.Weather{
+		Description: cc.WeatherDesc[0].Value,
+		Temperature: cc.Temp(unit),
+		Wind:        cc.Windspeed(unit),
 	}
 
-	return nil
-}
+	for i, weather := range wttr.Weather {
+		day, err := time.Parse(timeFormat, weather.Date)
+		if err != nil {
+			errJSON, _ := json.Marshal(model.ErrorMessage{Message: err.Error()})
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(errJSON)
+			return
+		}
 
-func notFound(weather *model.Weather) bool {
-	if len(weather.Description) == 0 {
-		return true
+		response.Forecast[i] = model.Forecast{
+			Day:         day.Weekday().String(),
+			Temperature: weather.Hourly[0].Temp(unit),
+			Wind:        weather.Hourly[0].Windspeed(unit),
+		}
 	}
 
-	return false
-}
-
-func toJSON(object interface{}) []byte {
-	respose, err := json.Marshal(object)
-	if err != nil {
-		fmt.Println(err)
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		errJSON, _ := json.Marshal(model.ErrorMessage{Message: err.Error()})
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(errJSON)
+		return
 	}
-	return respose
 }
