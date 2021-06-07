b57d5f544adc0308a461ca50d9968ce08d189955
cchalkias <cch@cchnet.gr>
2021-06-07 22:58:22 +0300

    Update weather.go

--- model/weather.go
+++ model/weather.go
@@ -2,6 +2,18 @@ package model
 
 // Weather entity
 type Weather struct {
+	FeelsLikeC      string `json:"FeelsLikeC"`
+	FeelsLikeF      string `json:"FeelsLikeF"`
+	Cloudcover      string `json:"cloudcover"`
+	Humidity        string `json:"humidity"`
+	ObservationTime string `json:"observation_time"`
+	PrecipMM        string `json:"precipMM"`
+	Pressure        string `json:"pressure"`
+	TempC           string `json:"temp_C"`
+	TempF           string `json:"temp_F"`
+	UvIndex         int    `json:"uvIndex"`
+	Visibility      string `json:"visibility"`
+	WeatherCode     string `json:"weatherCode"`	
 	Temperature string      `json:"temperature"`
 	Wind        string      `json:"wind"`
 	Description string      `json:"description"`
